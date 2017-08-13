94b4feaf777551d29d0badd4978914c00e71067c
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-08-13 12:06:31 +0000

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # RailsコンソールやIntegrationTest実行時に、carrierwaveで、
+    # NameError uninitialized-constant エラーが発生するのを回避するため
+    # see: http://qiita.com/h5y1m141@github/items/97e1941086c7f9f229ac  
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')] 
   end
 end
